f708719b45d711150632ea6264493f1b2f0a157e
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-06-14 10:08:20 +0530

    OOP Qestion 3

--- Full Stack/OOPS Activities/EmployeeTest.java
+++ Full Stack/OOPS Activities/EmployeeTest.java
@@ -40,17 +40,39 @@ public class EmployeeTest {
         return monthlySalary;
     }
 
-    // double displayYearlySalary()
-    // {
+    void displayYearlySalary()
+    {
+        double yearSalary = 0.0;
 
-    // }
+        //to get Yearly salary, We need to Multiply the Monthly Calary by 12 Because a Year Contains 12 months.
+        yearSalary = monthlySalary * 12;
+        System.out.println("The Yearly Salary of " + firstName + " " + lastName + " is " + yearSalary);
+    }
+
+    void salaryRaise()
+    {
+        //10% Increment in the Salary.
+        double raise = (monthlySalary / 10);
+        monthlySalary = monthlySalary + raise;
+        System.out.println("The Raise in Salary is: " + raise);
+    }
 
     
 
     //Main Function
     public static void main(String[] args)
     {
+        EmployeeTest E1 = new EmployeeTest("Gopal", "Agarwal", 60000);
+        EmployeeTest E2 = new EmployeeTest("Rajan", "Chetri", 100000);
 
+        E1.displayYearlySalary();
+        E1.salaryRaise();
+        E1.displayYearlySalary();
+
+        E2.displayYearlySalary();
+        E2.salaryRaise();
+        E2.displayYearlySalary();
+        
     }
 
 }
