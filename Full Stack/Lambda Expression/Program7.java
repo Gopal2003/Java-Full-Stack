e780da06de8546b750e4b76e5b72e258ed57f828
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-06-18 12:50:29 +0530

    Stream

--- Full Stack/Lambda Expression/Program7.java
+++ Full Stack/Lambda Expression/Program7.java
@@ -24,9 +24,6 @@ public class Program7 {
         */
     
         //Printing All Elements.
-    
-        // result.stream().forEach(System.out::println);
-    
         result.stream().forEach(System.out::println);
     }
     
