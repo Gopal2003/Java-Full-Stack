b3c9032091419cc1f6bb4d72ed93e2313efe35d9
Gopal <gopalmittal2003@gmail.com>
2024-07-08 12:58:34 +0530

    CRUD Operations

--- Full Stack/SpringBoot/demo-5/src/main/java/com/example/demo/Controller.java
+++ Full Stack/SpringBoot/demo-5/src/main/java/com/example/demo/Controller.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -92,4 +98,98 @@ public class Controller {
 			return null;
 		}
 	}
+	
+	@GetMapping("/generator/{number}")
+	public String generateTable(@PathVariable int number)
+	{
+		String rawHTML = "<h2> Table of " + number + " is </h2><hr/>";
+		for(int i = 1; i <= 10; i++)
+		{
+			int val = i * number;
+			rawHTML += "<p> " + number + " * " + i + "= " + (val) +" </p>";
+		}
+		
+		return rawHTML;
+	}
+	
+	ArrayList<User> aL = new ArrayList<>();
+	//Post Request.
+	@PostMapping("/adduser")
+	public String createUser(@RequestBody User user)
+	{
+		aL.add(user);
+		return "User Created: "+ user.getName();
+	}
+	
+	@GetMapping(value="/getuser", produces="application/json")
+	public List<User> getUsers()
+	{
+		List<User> users = new ArrayList<>();
+		int userCount = 1;
+		
+		//Iterating over the loop to get the user details.
+		for(User u : aL)
+		{
+			users.add(new User(u.getId(),u.getName(),u.getEmail(),u.getPassword()));
+//			users.add("user: " + userCount); 
+//			users.add("UserID: " + Integer.toString(u.getId()));
+//			users.add("UserName: " + u.getName());
+//			users.add("UserEmail: " + u.getEmail());
+//			users.add("UserPassword: " + u.getPassword());
+//			
+//			userCount++;
+		}
+		
+		
+		
+		return users;
+	}
+	
+	
+	// PUT Request to Server
+	//Put Request is sent to server for updating Existing User.
+	@PutMapping("/updateuser/{id}")
+	public String updateUser(@PathVariable int id,@RequestBody User user)
+	{
+		//Iterating over the ArrayList to find the object with the mentioned ID.
+		for(User u : aL)
+		{
+			if(u.getId() == id)
+			{
+				u.setId(user.getId());
+				u.setName(user.getName());
+				u.setEmail(user.getEmail());
+				u.setPassword(user.getPassword());
+			}
+		}
+		return "User Update: " + user.getName();
+	}
+	
+	//DELETE Request to Server
+	//Delete Reqeuest is Sent to Server for Deleting / Removing an Existing Object.
+	@DeleteMapping("/deleteuser/{id}")
+	public String deleteUser(@PathVariable int id)//,@RequestBody User user
+	{
+		//Index Variable to delete the object from the ArrayList.
+		int index = 0;
+		//Iterating over the ArrayList to find the object with the mentioned ID.
+		for(User u : aL)
+		{
+			if(u.getId() == id)
+			{
+				//Performing remove operation in ArrayList which removes the object which matches the mentioned id from particular index. 
+				aL.remove(index);
+				break;
+			}
+			index++;
+		}
+		
+		return "User Deleted Successfully";
+	}
+	
+	//CRUD Operations:
+	//Create: Post
+	//Read: Get
+	//Update: Put
+	//Delete: Delete.
 }
